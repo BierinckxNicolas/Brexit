4f2c7fd8badb2615ab397a5b3a0437fea18c350e
BierinckxNicolas <nicolas.bierinckx@student.ap.be>
2019-03-25 17:05:01 +0100

    miggrations & screenshot
    migrated and added screenshot

--- brexit/brexittime/views.py
+++ brexit/brexittime/views.py
@@ -4,9 +4,7 @@ from django.template import loader
 from .models import Clock
 
 # Create your views here.
-# Create your views here.
-#def index(request):
-#  return HttpResponse("Hello, world. You're at the brexit index.")
+
 def index(request):
     time_left = Clock.timediff
     return render(request, 'brexittime/index.html', {
