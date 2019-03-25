8e73136a2df00d002292f4a49e4bb95518984c76
BierinckxNicolas <nicolas.bierinckx@student.ap.be>
2019-03-25 16:27:56 +0100

    Model & view
    added model, view, html. not functioning

--- brexit/brexittime/models.py
+++ brexit/brexittime/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
 # Create your models here.
+
+
+class Clock(models.Model):    
+    clock_time = models.DateTimeField(auto_now_add= True)
+    timediff = models.DateTimeField()
+    def __str__(self):
+        return self.timediff
+    def time_left_till_brexit(self):
+        if self.clock_time:
+         brexit_time = datetime.datetime(2019, 3, 29, 23, 0, 0)
+         timediff = brexit_time - self.clock_time
+        return timediff
--- brexit/brexittime/views.py
+++ brexit/brexittime/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Clock
 
 # Create your views here.
 # Create your views here.
+#def index(request):
+#  return HttpResponse("Hello, world. You're at the brexit index.")
 def index(request):
-  return HttpResponse("Hello, world. You're at the brexit index.")
+    time_left = Clock.timediff
+    return render(request, 'brexittime/index.html', {
+        'time_left': time_left
+    })
+
+    
